10722aa8371e9407cb83b708b57545ed1f514c70
Daniel Villano-Herrera <danielvillanoh01@gmail.com>
2021-07-30 10:02:12 -0400

    Done primary and questions

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-# name:
-# author:
+# name: Daniel Villano-Herrera
+# date: 7/27/2021
 
 # -------------------- Section 1 ------------------------- #
 # ------------------ List Creation ----------------------- #
@@ -15,6 +15,10 @@ print('Creating an Empty List' '\n')
 #   2. Print the lists.
 #
 # WRITE CODE BELOW
+empty_list_dis = []
+empty_list_func = list()
+print(empty_list_dis)
+print(empty_list_func)
 
 
 print('\n' 'Creating a Pre-Populated List' '\n')
@@ -34,6 +38,18 @@ print('\n' 'Creating a Pre-Populated List' '\n')
 #
 # WRITE CODE BELOW
 integers = [1, 15, -4, -26, 34]
+floats = [1.3, 5.6, 9.5, 3.5, 4.3]
+booleans = [True, False, True]
+animals = ['Cat', 'Dog', 'Hamster']
+objects = ['car', 8, False]
+star = list('Betelgeuse')
+
+print(integers)
+print(floats)
+print(booleans)
+print(animals)
+print(objects)
+print(star)
 
 
 # -------------------- Section 2 ------------------------- #
@@ -54,7 +70,24 @@ print('Accessing and Modifying a List' '\n')
 #   1.a has been done for you.
 #
 # WRITE CODE BELOW
+
+# 1.a
 integers[2] = 44
+# 1.b
+floats[4] = 9.1
+# 1.c
+booleans[0] = False
+# 1.d
+animals[0] = 'Deer'
+# 1.e
+objects[1] = False
+
+# 2
+print(integers)
+print(floats)
+print(booleans)
+print(animals)
+print(objects)
 
 
 print('\n' 'Append, Insert, and Remove' '\n')
@@ -72,8 +105,24 @@ print('\n' 'Append, Insert, and Remove' '\n')
 #   1.a has been done for you.
 #
 # WRITE CODE BELOW
-integers.append(25)
 
+# 1.a
+integers.append(25)
+# 1.b
+floats.append(3.4)
+# 1.c
+booleans.remove(False)
+# 1.d
+animals.insert(0, 'Cat')
+# 1.e
+objects.insert(2, 35)
+
+# 2
+print(integers)
+print(floats)
+print(booleans)
+print(animals)
+print(objects)
 
 print('\n' 'List Concatenation' '\n')
 # 3. List Concatenation
@@ -99,6 +148,13 @@ print(
     f'example_duplication | {example_duplication}' '\n'
 )
 
+float_integer_concatenation = floats + integers
+duplicate_animals = animals * 3
+print(
+    '\n'
+    f'float_integer_concatenation | {float_integer_concatenation}' '\n'
+    f'duplicate_animals  | {duplicate_animals }' '\n'
+)
 
 # -------------------- Section 3 ------------------------- #
 # --------------------- Looping -------------------------- #
@@ -115,8 +171,12 @@ print('Looping' '\n')
 #   An example has been shown below:
 #
 # WRITE CODE BELOW
-
-
+# via in range()
+for i in range(len(float_integer_concatenation)):
+    print(float_integer_concatenation[i])
+print()
+for item in animals:
+    print(item)
 # -------------------- Section 4 ------------------------- #
 # ------------------ Comprehension ----------------------- #
 print('\n' '# -------------------- Section 3 ------------------------- #')
@@ -142,9 +202,38 @@ from random import randint
 size = 5
 rolls = []
 
+# Size 5
 for i in range(size):
-    pass  # finish the loop
+    dice = randint(1, 6)
+    rolls.append(dice)
 
 print(f'rolls | {rolls}')
-print(f'1\t| total - {rolls.count(1)}\t\t| rate of appearance - {"{:.2%}".format(rolls.count(1) / size)}')
-# finish the rest!
+print(f'1\t| total - {rolls.count(3)}\t\t| rate of appearance - {"{:.2%}".format(rolls.count(3) / size)}')
+# finish the rest!'
+
+# Size 10
+size = 10
+rollsTwo = []
+for i in range(size):
+    dice = randint(1, 6)
+    rollsTwo.append(dice)
+print(f'rolls | {rollsTwo}')
+print(f'1\t| total - {rollsTwo.count(3)}\t\t| rate of appearance - {"{:.2%}".format(rollsTwo.count(3) / size)}')
+
+# Size 100
+size = 100
+rollsThree = []
+for i in range(size):
+    dice = randint(1, 6)
+    rollsThree.append(dice)
+print(f'rolls | {rollsThree}')
+print(f'1\t| total - {rollsThree.count(3)}\t\t| rate of appearance - {"{:.2%}".format(rollsThree.count(3) / size)}')
+
+#Size 1000
+size = 1000
+rollsFour = []
+for i in range(size):
+    dice = randint(1, 6)
+    rollsFour.append(dice)
+print(f'rolls | {rollsFour}')
+print(f'1\t| total - {rollsFour.count(3)}\t\t| rate of appearance - {"{:.2%}".format(rollsFour.count(3) / size)}')
